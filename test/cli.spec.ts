ea39880b0c76c8520a3a6b1450a1cb3d41d6642b
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-04-12 10:13:22 +0100

    style: :art: Fmt

--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -82,7 +82,6 @@ describe("CLI Output Test Suite", () => {
   });
 
   describe("Command: <check>", () => {
-    
     it("should validate an upgrade", async () => {
       const { stdout } = await execAsync(
         "pnpm validate check test-upgrade-mini --directory reference"
