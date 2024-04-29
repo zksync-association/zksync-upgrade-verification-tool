e5c83e466a2cad23c278f45b048cf15d1007e693
migue <miguelfeliped@gmail.com>
2024-04-29 18:56:44 -0300

    improved env builder

--- cli/src/lib/env-builder.ts
+++ cli/src/lib/env-builder.ts
@@ -4,10 +4,14 @@ import {GithubClient} from "./github-client.js";
 import {RpcClient} from "./rpc-client.js";
 
 export class EnvBuilder {
-  etherscanKey?: string
+  _etherscanKey?: string
   githubKey?: string
   rpcUrl?: string
   _network?: Network
+
+  _l1Client?: BlockExplorerClient
+  _l2Client?: BlockExplorerClient
+  _github?: GithubClient
 
   withNetwork(n: Network): void {
     this._network = n
@@ -20,8 +24,15 @@ export class EnvBuilder {
     return this._network
   }
 
+  get etherscanKey (): string {
+    if (!this._etherscanKey) {
+      throw new Error('Missing etherscan key')
+    }
+    return this._etherscanKey
+  }
+
   withEtherscanKey(etherscanKey: string): void {
-    this.etherscanKey = etherscanKey
+    this._etherscanKey = etherscanKey
   }
 
   withGithubKey(maybeKey: string | undefined): void {
@@ -33,27 +44,32 @@ export class EnvBuilder {
   }
 
   l1Client (): BlockExplorerClient {
-    if (!this.etherscanKey) {
-      throw new Error('Missing etherscan key')
+    if (this._l1Client) {
+      return this._l1Client
     }
-    if (!this._network) {
-      throw new Error('Missing network')
-    }
-    return BlockExplorerClient.fromNetwork(this.etherscanKey, this._network)
+
+    this._l1Client = BlockExplorerClient.fromNetwork(this.etherscanKey, this.network);
+    return this._l1Client
   }
 
   l2Client (): BlockExplorerClient {
-    return BlockExplorerClient.forL2()
+    if (this._l2Client) {
+      return this._l2Client
+    }
+    this._l2Client = BlockExplorerClient.forL2()
+    return this._l2Client
   }
 
   github (): GithubClient {
-    return new GithubClient(this.githubKey)
+    if (this._github) {
+      return this._github
+    }
+
+    this._github = new GithubClient(this.githubKey);
+    return this._github
   }
 
   rpc (): RpcClient {
-    if (!this._network) {
-      throw new Error('Missing network')
-    }
-    return new RpcClient(this._network, this.rpcUrl)
+    return new RpcClient(this.network, this.rpcUrl)
   }
 }
