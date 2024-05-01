37ddcbcffb450457623e7a9b8bafe2889c1c1647
migue <miguelfeliped@gmail.com>
2024-05-01 16:44:43 -0300

    try to change reporter in ci

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["./vitest-setup.js"],
+    reporters: ["default"],
     testTimeout: 30000,
     fileParallelism: false,
     sequence: {
