7c43fc267a35e209edf98242e4554fd7920c8ba5
hojarasca <miguel@moonsonlabs.com>
2024-05-24 17:21:00 -0300

    fmt fixes

--- cli/src/lib/github-client.ts
+++ cli/src/lib/github-client.ts
@@ -14,7 +14,7 @@ export class GithubClient {
 
   static async create(ref: string): Promise<GithubClient> {
     const repo = await EraContractsRepo.default();
-    await repo.init()
+    await repo.init();
     return new GithubClient(repo, ref);
   }
 
