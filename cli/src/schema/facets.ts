499975e0d6f7c172eff343d33badcfdd035008a8
hojarasca <miguel@moonsonlabs.com>
2024-05-24 16:41:40 -0300

    testing bigger upgrade

--- cli/src/schema/facets.ts
+++ cli/src/schema/facets.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
 import { account20String, bytes32Hash } from "../schema";
 
-// const facetNames = z.enum(["ExecutorFacet", "AdminFacet", "GettersFacet", "MailboxFacet"]);
-const facetNames = z.string();
 export const facetsSchema = z.record(
-  facetNames,
+  z.string(),
   z.object({
     address: account20String,
     txHash: bytes32Hash,
--- cli/src/schema/l2Upgrade.ts
+++ cli/src/schema/l2Upgrade.ts
@@ -10,27 +10,7 @@ const systemContracts = z.array(
 );
 
 export const l2UpgradeSchema = z.object({
-  systemContracts,
-  defaultAA: z.object({
-    name: z.literal("DefaultAccount"),
-    bytecodeHashes: z.array(bytes32Hash),
-  }),
-  bootloader: z.object({
-    name: z.literal("Bootloader"),
-    bytecodeHashes: z.array(bytes32Hash),
-  }),
-  forcedDeployments: z.array(
-    z.object({
-      bytecodeHash: bytes32Hash,
-      newAddress: account20String,
-      value: z.number(),
-      input: hashString,
-      callConstructor: z.boolean(),
-    })
-  ),
-  forcedDeploymentCalldata: hashString,
-  calldata: hashString,
-  tx: transactionSchema,
+  systemContracts
 });
 
 export type L2UpgradeJson = z.infer<typeof l2UpgradeSchema>;
