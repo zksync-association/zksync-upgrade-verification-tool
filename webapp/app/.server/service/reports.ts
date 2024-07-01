59f5e0cd2b2134c8557957cbd8cf426640ee0697
aon <21188659+aon@users.noreply.github.com>
2024-07-01 12:21:38 -0300

    fix: comment out reports.ts for now

--- webapp/app/.server/service/reports.ts
+++ webapp/app/.server/service/reports.ts
@@ -1,45 +1,45 @@
-import * as process from "node:process";
-import {
-  BlockExplorerClient,
-  CheckReport,
-  FileSystem,
-  GitContractsRepo,
-  RpcClient,
-  UpgradeImporter,
-  ZkSyncEraDiff,
-  ZksyncEraState,
-} from "validate-cli";
-import { hexToBytes } from "viem";
+// import * as process from "node:process";
+// import {
+//   BlockExplorerClient,
+//   CheckReport,
+//   FileSystem,
+//   GitContractsRepo,
+//   RpcClient,
+//   UpgradeImporter,
+//   ZkSyncEraDiff,
+//   ZksyncEraState,
+// } from "validate-cli";
+// import { hexToBytes } from "viem";
 
-export async function checkReport(upgradeDir: string, repoPath: string): Promise<string> {
-  const network = "mainnet";
-  const apiKey = process.env.ETHERSCAN_API_KEY;
-  if (!apiKey) {
-    throw new Error("No api key");
-  }
-  const l1Explorer = BlockExplorerClient.forL1(apiKey, network);
-  const l2Explorer = BlockExplorerClient.forL2(network);
-  const l1Rpc = RpcClient.forL1(network);
-  const current = await ZksyncEraState.fromBlockchain(network, l1Explorer, l1Rpc);
+// export async function checkReport(upgradeDir: string, repoPath: string): Promise<string> {
+//   const network = "mainnet";
+//   const apiKey = process.env.ETHERSCAN_API_KEY;
+//   if (!apiKey) {
+//     throw new Error("No api key");
+//   }
+//   const l1Explorer = BlockExplorerClient.forL1(apiKey, network);
+//   const l2Explorer = BlockExplorerClient.forL2(network);
+//   const l1Rpc = RpcClient.forL1(network);
+//   const current = await ZksyncEraState.fromBlockchain(network, l1Explorer, l1Rpc);
 
-  const fileSystem = new FileSystem();
-  const importer = new UpgradeImporter(fileSystem);
+//   const fileSystem = new FileSystem();
+//   const importer = new UpgradeImporter(fileSystem);
 
-  const upgradeFiles = await importer.readFromFiles(upgradeDir, network);
+//   const upgradeFiles = await importer.readFromFiles(upgradeDir, network);
 
-  const buff = Buffer.from(hexToBytes(upgradeFiles.upgradeCalldataHex.unwrap()));
-  const [proposed, sysAddresses] = await ZksyncEraState.fromCalldata(
-    buff,
-    network,
-    l1Explorer,
-    l1Rpc,
-    l2Explorer
-  );
+//   const buff = Buffer.from(hexToBytes(upgradeFiles.upgradeCalldataHex.unwrap()));
+//   const [proposed, sysAddresses] = await ZksyncEraState.fromCalldata(
+//     buff,
+//     network,
+//     l1Explorer,
+//     l1Rpc,
+//     l2Explorer
+//   );
 
-  const diff = new ZkSyncEraDiff(current, proposed, sysAddresses);
+//   const diff = new ZkSyncEraDiff(current, proposed, sysAddresses);
 
-  const repo = new GitContractsRepo(repoPath);
+//   const repo = new GitContractsRepo(repoPath);
 
-  const report = new CheckReport(diff, repo, l1Explorer);
-  return report.format();
-}
+//   const report = new CheckReport(diff, repo, l1Explorer);
+//   return report.format();
+// }
