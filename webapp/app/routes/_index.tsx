fc4c3376a1a273ee66db5f9a270680c049fd9109
aon <21188659+aon@users.noreply.github.com>
2024-07-01 17:30:35 -0300

    feat: improve with margins and better text displays

--- webapp/app/routes/_index.tsx
+++ webapp/app/routes/_index.tsx
@@ -1,6 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import zksync from "@/images/zksync.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { MetaFunction } from "@remix-run/node";
+import { useAccount } from "wagmi";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,18 +12,33 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { address } = useAccount();
+
   return (
     <main className="flex flex-col items-center">
-      <img src={zksync} alt="zkSync" className="w-64 h-64 mx-auto" />
-      <h1 className="text-4xl text-primary font-semibold">
+      <img src={zksync} alt="zkSync" className="mx-auto h-48 w-48" />
+      <h1 className="font-semibold text-4xl text-primary">
         zkSync Era Upgrade Analysis & Voting Tool
       </h1>
       <p className="mt-10 text-lg">
         Analyze upgrade proposal transaction call data in human-readble format and cast your vote.
       </p>
-      <p className="mt-1 text-lg">Please connect your wallet to continue.</p>
-      <div className="mt-10">
-        <ConnectButton />
+
+      <AspectRatio
+        ratio={3 / 2}
+        className="mt-10 flex items-center justify-center rounded-md border-2 border-dashed bg-muted/20"
+      >
+        {address ? (
+          <p>
+            You have <span className="text-primary">3</span> available proposals.
+          </p>
+        ) : (
+          <p className="text-muted-foreground">Please connect your wallet to continue.</p>
+        )}
+      </AspectRatio>
+
+      <div className="absolute bottom-10 animate-slide-up">
+        <ConnectButton showBalance={false} />
       </div>
     </main>
   );
--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -67,10 +67,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-up": {
+          "0%": { opacity: "0", transform: "translateY(100%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-up": "slide-up 0.3s ease-in-out forwards",
       },
     },
   },
