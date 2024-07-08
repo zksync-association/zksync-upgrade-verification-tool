372d288d16bdc32e428dd9986d6542fcdb1c6772
hojarasca <miguel@moonsonglabs.com>
2024-07-08 17:14:51 -0300

    using new env var name

--- webapp/app/.server/service/clients.ts
+++ webapp/app/.server/service/clients.ts
@@ -2,5 +2,5 @@ import { env } from "@config/env.server";
 import { BlockExplorerClient, RpcClient } from "validate-cli";
 
 export const l1Explorer = BlockExplorerClient.forL1(env.ETHERSCAN_API_KEY, env.ETH_NETWORK);
-export const l1Rpc = new RpcClient(env.L1_RPC_CLI);
+export const l1Rpc = new RpcClient(env.L1_RPC_URL);
 export const l2Explorer = BlockExplorerClient.forL2(env.ETH_NETWORK);
