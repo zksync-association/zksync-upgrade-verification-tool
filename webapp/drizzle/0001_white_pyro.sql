8a52a3e9759ad24ed8b2fb1d6704563c9d793075
hojarasca <miguel@moonsonglabs.com>
2024-07-08 16:52:20 -0300

    removed not needed migration

--- webapp/drizzle/0001_white_pyro.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "upgrades" ADD COLUMN "upgrade_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "upgrades" ADD COLUMN "started_tx_hash" text;--> statement-breakpoint
-ALTER TABLE "upgrades" ADD COLUMN "finished_tx_hash" text;--> statement-breakpoint
-ALTER TABLE "upgrades" DROP COLUMN IF EXISTS "name";--> statement-breakpoint
-ALTER TABLE "upgrades" DROP COLUMN IF EXISTS "status";--> statement-breakpoint
-ALTER TABLE "upgrades" ADD CONSTRAINT "upgrades_upgrade_id_unique" UNIQUE("upgrade_id");
