b9c564bd494341602f543d8088b39c21a094c995
hojarasca <miguel@moonsonglabs.com>
2024-07-10 00:58:15 -0300

    removed useless commented code

--- webapp/app/.server/db/dto/signatures.ts
+++ webapp/app/.server/db/dto/signatures.ts
@@ -9,19 +9,3 @@ export async function createOrIgnoreSignature(
 ): Promise<void> {
   await createOrIgnoreRecord(signaturesTable, data, { tx });
 }
-
-// export async function getProposalByExternalId(externalId: string) {
-//   return getFirst(
-//     await db.select().from(proposalsTable).where(eq(proposalsTable.externalId, externalId))
-//   );
-// }
-//
-// export async function updateProposal(
-//   data: Partial<InferInsertModel<typeof proposalsTable>> & {
-//     id: InferSelectModel<typeof proposalsTable>["id"];
-//   }
-// ) {
-//   return getFirstOrThrow(
-//     await db.update(proposalsTable).set(data).where(eq(proposalsTable.id, data.id)).returning()
-//   );
-// }
