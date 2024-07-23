a0341cd176825fcad7f25ecd82553d1f8e5b86e3
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-07-23 14:54:49 +0100

    feat: :lipstick: Env  Badge
    new status badge for connected environment

---
+++ webapp/app/components/env-badge.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import type { NodeEnv } from "@config/env.server";
+
+export default function EnvBadge({ environment }: { environment: NodeEnv }) {
+  if (environment === "production") {
+    return null;
+  }
+  return (
+    <Badge variant={"outline"} className="h-auto px-3 py-1 text-s">
+      {environment}
+    </Badge>
+  );
+}
--- webapp/app/components/navbar.tsx
+++ webapp/app/components/navbar.tsx
@@ -1,13 +1,18 @@
+import EnvBadge from "@/components/env-badge";
 import Logo from "@/components/logo";
+import type { NodeEnv } from "@config/env.server";
 import { $path } from "remix-routes";
 
-export default function Navbar() {
+export default function Navbar({ environment }: { environment: NodeEnv }) {
   return (
     <header className="w-full">
-      <nav>
+      <nav className="flex justify-between">
         <a href={$path("/")}>
           <Logo />
         </a>
+        <div className="flex items-center space-x-2">
+          <EnvBadge environment={environment} />
+        </div>
       </nav>
     </header>
   );
