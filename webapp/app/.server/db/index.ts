7aa1b9b7407cab2fc1f5ebca3aa19b74d0a03ee2
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-07-23 16:44:22 +0100

    feat: :bug: Added DB error messages on startup

--- webapp/app/.server/db/index.ts
+++ webapp/app/.server/db/index.ts
@@ -1,4 +1,5 @@
 import * as schema from "@/.server/db/schema";
+import { proposalsTable } from "@/.server/db/schema";
 import { env } from "@config/env.server";
 import { defaultLogger } from "@config/log.server";
 import { drizzle } from "drizzle-orm/postgres-js";
@@ -15,3 +16,20 @@ export const db = drizzle(queryClient, {
     },
   },
 });
+
+async function testDbConnection() {
+  try {
+    await db.select().from(proposalsTable).limit(1);
+
+    console.log("Connected to the database successfully");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("An error occurred while connecting to the database:", error.name);
+      console.error(error.message);
+      console.error(error.cause);
+    }
+    throw new Error("Failed to connect to the database");
+  }
+}
+
+await testDbConnection();
--- webapp/app/test/unit/navbar.test.tsx
+++ webapp/app/test/unit/navbar.test.tsx
@@ -11,20 +11,16 @@ describe("Navbar", () => {
   it("renders Navbar with Logo and correct link", async () => {
     render(<Navbar environment={"production"} />);
 
-    // Check for the logo
     const logo = await screen.findByAltText("Zksync Logo");
     expect(logo).toBeInTheDocument();
     expect(logo).toHaveAttribute("src", expect.stringContaining("zksync.svg"));
 
-    // Check for the link
     const link = screen.getByRole("link");
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute("href", "/");
 
-    // Check that the logo is inside the link
     expect(link).toContainElement(logo);
 
-    // Check the header and nav structure
     const header = screen.getByRole("banner");
     expect(header).toBeInTheDocument();
     expect(header).toContainElement(screen.getByRole("navigation"));
