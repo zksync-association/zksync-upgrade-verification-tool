ed8ae72d61d5e3d95ac589cdb634254db9e85047
migue <miguelfeliped@gmail.com>
2024-07-25 16:29:23 -0300

    Removed commented code

--- webapp/app/test/unit/proposal-state.test.tsx
+++ webapp/app/test/unit/proposal-state.test.tsx
@@ -31,23 +31,4 @@ describe("ProposalState", () => {
     expect(element).toHaveClass("text-yellow-400");
     expect(element).toHaveTextContent(/^LEGAL VETO PERIOD \(day 6 out of 13\)$/);
   });
-
-  // it("renders Navbar with Logo and correct link", async () => {
-  //   render(<ProposalState status={PROPOSAL_STATES.LegalVetoPeriod} times={null} />);
-  //
-  //   const element = await screen.findByText(/LEGAL VETO PERIOD/);
-  //   expect(element).toBeInTheDocument();
-  //   expect(element).toHaveClass("text-yellow-400")
-  // });
-
-  // it("renders EnvBadge for development environment but not for production", () => {
-  //   const { rerender } = render(<Navbar environment="development" />);
-  //
-  //   let envBadge: HTMLElement | null = screen.getByText("Dev");
-  //   expect(envBadge).toBeInTheDocument();
-  //
-  //   rerender(<Navbar environment="production" />);
-  //   envBadge = screen.queryByText("production");
-  //   expect(envBadge).not.toBeInTheDocument();
-  // });
 });
