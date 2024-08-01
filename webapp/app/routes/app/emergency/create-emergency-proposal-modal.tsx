e6409d3d964e3b66448061fd64a1567e13f626c3
hojarasca <miguel@moonsonglabs.com>
2024-08-01 01:06:27 -0300

    fixed import pointing to wrong file after merge

--- webapp/app/routes/app/emergency/create-emergency-proposal-modal.tsx
+++ webapp/app/routes/app/emergency/create-emergency-proposal-modal.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import type { action } from "@/routes/app/emergency/_route";
+import type { action } from "@/routes/app/emergency/_index/_route";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { useFetcher } from "@remix-run/react";
