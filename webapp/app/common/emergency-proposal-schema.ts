1b243e60426ee4c4112c835020b1a287bac6ea4b
migue <miguelfeliped@gmail.com>
2024-08-13 19:24:14 -0300

    simplified schema

--- webapp/app/common/emergency-proposal-schema.ts
+++ webapp/app/common/emergency-proposal-schema.ts
@@ -1,13 +1,8 @@
-import { addressSchema, hexSchema } from "@/common/basic-schemas";
-import { callSchema } from "@/common/calls";
+import { addressSchema } from "@/common/basic-schemas";
 import { padHex } from "viem";
 import { z } from "zod";
 
-export const basicPropSchema = z.object({
-  salt: hexSchema,
-});
-
-export const emergencyPropSchema = basicPropSchema.extend({
+export const fullEmergencyPropSchema = z.object({
   title: z.string().min(1, "Title is required"),
   salt: z
     .string()
@@ -16,12 +11,7 @@ export const emergencyPropSchema = basicPropSchema.extend({
       message: "Salt must be a 32-byte hex string (64 characters)",
     })
     .default(padHex("0x0")),
-});
-
-export const fullEmergencyPropSchema = emergencyPropSchema.extend({
   proposer: addressSchema,
 });
-
-export type EmergencyProp = z.infer<typeof emergencyPropSchema>;
 
 export type FullEmergencyProp = z.infer<typeof fullEmergencyPropSchema>;
