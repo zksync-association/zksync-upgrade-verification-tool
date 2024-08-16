f45b40cbbfb3d5ea9be73691e484f446059de2cd
aon <21188659+aon@users.noreply.github.com>
2024-08-16 11:51:38 -0300

    feat: improve auth schemas

--- webapp/server/middlewares/auth.ts
+++ webapp/server/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { getUserAuthRole } from "@/.server/service/authorized-users";
-import { hexSchema } from "@/common/basic-schemas";
+import { addressSchema, hexSchema } from "@/common/basic-schemas";
 import { readAuthSession } from "@server/utils/auth-session";
 import type { NextFunction, Request, Response } from "express";
 import { $path, type Routes } from "remix-routes";
@@ -20,14 +20,14 @@ function isProtectedRoute(req: Request) {
 export async function auth(req: Request, res: Response, next: NextFunction) {
   const session = readAuthSession(req);
 
-  const parsed = hexSchema.safeParse(session.address);
-  if (parsed.error) {
+  const address = addressSchema.safeParse(session.address);
+  if (!address.success) {
     clearUserHeaders(req);
     return next();
   }
 
-  const role = await getUserAuthRole(hexSchema.parse(parsed.data));
-  setUserHeaders(req, { address: parsed.data, role });
+  const role = await getUserAuthRole(hexSchema.parse(address.data));
+  setUserHeaders(req, { address: address.data, role });
 
   if (isProtectedRoute(req) && role === "visitor") {
     return res.redirect($path("/app/denied"));
