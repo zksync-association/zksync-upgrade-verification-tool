c1809ccb42f7b1d1a84be5fb67dd414e6b2ecc67
hojarasca <miguel@moonsonglabs.com>
2024-09-02 10:52:03 -0300

    Avoid magical regex on number parsing.

--- webapp/app/common/basic-schemas.ts
+++ webapp/app/common/basic-schemas.ts
@@ -2,7 +2,6 @@ import { type Hex, getAddress } from "viem";
 import { z } from "zod";
 
 const hexRegexp = /^0x[0-9a-fA-F]*$/;
-const numericalStrRegex = /^[0-9]+$/;
 
 const hexChars = z.string().refine((str) => hexRegexp.test(str), "Not a valid hex");
 
@@ -11,9 +10,9 @@ export const addressSchema = hexChars
   .refine((str) => str.length === 42, "Address have to be 20 bytes long")
   .transform((str) => getAddress(str));
 
-export const numericStrSchema = z
-  .string()
-  .regex(numericalStrRegex, { message: "Must be a number" });
+export const numericStrSchema = z.coerce
+  .bigint({ message: "Must be a integer number" })
+  .transform((num) => num.toString());
 
 export const nonZeroNumericStrSchema = numericStrSchema.refine((s) => !/^0+$/.test(s), {
   message: "Cannot be zero.",
