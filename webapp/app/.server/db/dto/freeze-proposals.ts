f56ae8f9218ab1b66e9eee11c4f391ed6a0919c4
hojarasca <miguel@moonsonglabs.com>
2024-09-02 11:59:44 -0300

    Removed unique constrait in freezes.
    Instead replaced with code checking.

--- webapp/app/.server/db/dto/freeze-proposals.ts
+++ webapp/app/.server/db/dto/freeze-proposals.ts
@@ -1,9 +1,26 @@
 import { db } from "@/.server/db";
 import { getFirst, getFirstOrThrow } from "@/.server/db/dto/utils/common";
+import { ValidationError } from "@/.server/db/errors";
 import { freezeProposalsTable } from "@/.server/db/schema";
-import { type InferInsertModel, type InferSelectModel, eq } from "drizzle-orm";
+import { type InferInsertModel, type InferSelectModel, and, eq } from "drizzle-orm";
 
 export async function createFreezeProposal(data: InferInsertModel<typeof freezeProposalsTable>) {
+  const existing = await db
+    .select()
+    .from(freezeProposalsTable)
+    .where(
+      and(
+        eq(freezeProposalsTable.externalId, data.externalId),
+        eq(freezeProposalsTable.type, data.type)
+      )
+    );
+
+  const someIsNotExpired = existing.some((row) => row.validUntil.valueOf() > new Date().valueOf());
+
+  if (someIsNotExpired) {
+    throw new ValidationError(`There is already an active ${data.type} proposal.`);
+  }
+
   return await db.insert(freezeProposalsTable).values(data).returning().then(getFirstOrThrow);
 }
 
--- webapp/app/.server/db/errors.ts
+++ webapp/app/.server/db/errors.ts
@@ -3,3 +3,9 @@ import postgres from "postgres";
 export function isUniqueConstraintError(err: unknown) {
   return err instanceof postgres.PostgresError && err.code === "23505";
 }
+
+export class ValidationError extends Error {}
+
+export function isValidationError(error: any): boolean {
+  return error instanceof ValidationError;
+}
