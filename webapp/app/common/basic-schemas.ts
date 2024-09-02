e867d75daeacbaa7258785848a2b984776ba107a
hojarasca <miguel@moonsonglabs.com>
2024-09-02 12:34:37 -0300

    style fixes. running failing tests again.

--- webapp/app/common/basic-schemas.ts
+++ webapp/app/common/basic-schemas.ts
@@ -12,5 +12,5 @@ export const addressSchema = hexChars
 
 export const nonZeroNumericStrSchema = z.coerce
   .bigint({ message: "Must be a integer number" })
-  .refine(num => num !== 0n, { message: "Cannot be zero" })
+  .refine((num) => num !== 0n, { message: "Cannot be zero" })
   .transform((num) => num.toString());
