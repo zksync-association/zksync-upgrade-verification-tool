769a609d425e90c3f4f470f0925e3d4012ad97cf
aon <21188659+aon@users.noreply.github.com>
2024-09-09 11:24:54 -0300

    feat: improve cookie hook with parsing and additional comps

--- apps/web/app/components/hooks/use-user-role-cookie.ts
+++ apps/web/app/components/hooks/use-user-role-cookie.ts
@@ -1,6 +1,7 @@
 import {
   USER_ROLE_COOKIE_NAME,
   userRoleCookieOptions,
+  UserRoleCookieSchema,
   type UserRoleCookie,
 } from "@server/utils/user-role-cookie";
 import { useCookies } from "react-cookie";
@@ -8,14 +9,22 @@ import { useCookies } from "react-cookie";
 export default function useUserRoleCookie(): {
   cookie: UserRoleCookie | undefined;
   setCookie: (value: UserRoleCookie) => void;
+  removeCookie: () => void;
 } {
-  const [cookie, setCookie] = useCookies<
+  const [cookie, setCookie, removeCookie] = useCookies<
     typeof USER_ROLE_COOKIE_NAME,
     { [USER_ROLE_COOKIE_NAME]?: UserRoleCookie }
   >([USER_ROLE_COOKIE_NAME]);
 
+  let cookieValue = cookie[USER_ROLE_COOKIE_NAME];
+
+  if (!UserRoleCookieSchema.safeParse(cookieValue).success) {
+    cookieValue = undefined;
+  }
+
   return {
-    cookie: cookie[USER_ROLE_COOKIE_NAME],
+    cookie: cookieValue,
     setCookie: (value) => setCookie(USER_ROLE_COOKIE_NAME, value, userRoleCookieOptions),
+    removeCookie: () => removeCookie(USER_ROLE_COOKIE_NAME),
   };
 }
