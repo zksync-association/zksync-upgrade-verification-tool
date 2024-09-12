9b6b0910f14e49de8c0c684784267e258a90e56d
hojarasca <miguel@moonsonglabs.com>
2024-09-12 18:38:49 -0300

    Lint fixes

--- packages/e2e/suites/full/emergency-upgrade.spec.ts
+++ packages/e2e/suites/full/emergency-upgrade.spec.ts
@@ -1,7 +1,6 @@
 import { expect, type IntRange, type RoleSwitcher, test } from "./helpers/dappwright.js";
 import type { Page } from "@playwright/test";
 import type { Dappwright, Dappwright as Wallet } from "@tenkeylabs/dappwright";
-import type { COUNCIL_SIZE } from "@repo/contracts/helpers/constants";
 import { TestApp } from "./helpers/test-app.js";
 
 const testApp = new TestApp();
@@ -17,27 +16,27 @@ async function goToEmergencyIndex(page: Page) {
   await page.getByText("Active Emergency Proposals", { exact: true }).isVisible();
 }
 
-async function repeatFor<T>(
-  numbers: T[],
-  action: (n: T) => Promise<void>
-) {
+async function repeatFor<T>(numbers: T[], action: (n: T) => Promise<void>) {
   for (const n of numbers) {
-    await action(n)
+    await action(n);
   }
 }
 
-function genericRange<From extends number, To extends number>(from: IntRange<From, To>, to: IntRange<From, To>): IntRange<From, To>[] {
+function genericRange<From extends number, To extends number>(
+  from: IntRange<From, To>,
+  to: IntRange<From, To>
+): IntRange<From, To>[] {
   const res = [];
   let last = from;
   while (last <= to) {
-    res.push(last)
-    last = last + 1 as IntRange<From, To>;
+    res.push(last);
+    last = (last + 1) as IntRange<From, To>;
   }
-  return res as IntRange<From, To>[]
+  return res as IntRange<From, To>[];
 }
 
-const councilRange = genericRange<1, 12>
-const guardianRange = genericRange<1, 8>
+const councilRange = genericRange<1, 12>;
+const guardianRange = genericRange<1, 8>;
 
 async function goToCreateEmergencyProposal(page: Page) {
   await goToEmergencyIndex(page);
@@ -45,15 +44,17 @@ async function goToCreateEmergencyProposal(page: Page) {
   await page.getByText("Create new emergency proposal").isVisible();
 }
 
-async function createEmergencyProposal(page: Page, title="Emergency test!!") {
-  await goToCreateEmergencyProposal(page)
+async function createEmergencyProposal(page: Page, title = "Emergency test!!") {
+  await goToCreateEmergencyProposal(page);
 
   await page.getByLabel("Title").fill(title);
   await page.getByRole("button", { name: "Next" }).click();
 
   await page.getByText("Define upgrade calls").isVisible();
   await page.getByLabel("Target Address").fill("0x6fe12efa79da1426af9c811b80edca74556c5a0e");
-  await page.getByLabel("Calldata").fill("0x3fb5c1cb000000000000000000000000000000000000000000000000000000000000000c");
+  await page
+    .getByLabel("Calldata")
+    .fill("0x3fb5c1cb000000000000000000000000000000000000000000000000000000000000000c");
 
   await page.getByRole("button", { name: "Add call" }).click();
   await page.getByRole("button", { name: "Next" }).isEnabled();
@@ -66,12 +67,12 @@ async function createEmergencyProposal(page: Page, title="Emergency test!!") {
 async function goToActiveEmergencyProposal(page: Page) {
   await goToEmergencyIndex(page);
   await page.getByText("Title").isVisible();
-  const row = page.locator("tbody tr").first()
-  await row.getByText("View").click()
+  const row = page.locator("tbody tr").first();
+  await row.getByText("View").click();
 }
 
 function approveButton(page: Page) {
-  return page.getByTestId("approve-button")
+  return page.getByTestId("approve-button");
 }
 
 async function approveEmergencyProposal(page: Page, wallet: Wallet) {
@@ -85,49 +86,53 @@ async function extractApprovalCount(page: Page, testId: string) {
   if (!initialApprovals) {
     throw new Error(`No approval count found with id: ${testId}`);
   }
-  return initialApprovals
+  return initialApprovals;
 }
 
-async function withVoteIncrease(page: Page, testId: string , fn: () => Promise<void>) {
+async function withVoteIncrease(page: Page, testId: string, fn: () => Promise<void>) {
   const initialApprovals = await extractApprovalCount(page, testId);
   const initialCount = Number.parseInt(initialApprovals.split("/")[0] ?? "");
 
   await fn();
 
-  await expect(page.getByTestId(testId)).toHaveText(
-    new RegExp(`${initialCount + 1}\/\\d`)
-  );
+  await expect(page.getByTestId(testId)).toHaveText(new RegExp(`${initialCount + 1}\/\\d`));
 }
 
 test("TC213: Go to / -> Emergency upgrade button is enabled", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByText("Emergency Upgrades")).toBeEnabled();
-})
+});
 
-test("TC201: Go to index page, no proposals -> active and inactive emergency upgrades empty", async ({ page }) => {
+test("TC201: Go to index page, no proposals -> active and inactive emergency upgrades empty", async ({
+  page,
+}) => {
   await goToEmergencyIndex(page);
 
   await expect(page.getByText("No active emergency proposals found.")).toBeVisible();
   await expect(page.getByText("No inactive emergency proposals found.")).toBeVisible();
-})
+});
 
-test("TC202: Create a valid emergency proposal -> List it into active proposals", async ({ page }) => {
+test("TC202: Create a valid emergency proposal -> List it into active proposals", async ({
+  page,
+}) => {
   await createEmergencyProposal(page, "TC202");
   await goToEmergencyIndex(page);
   await page.getByText("title").isVisible();
-  const rows = await page.locator("tbody tr").all()
-  expect(rows.length).toBe(1)
+  const rows = await page.locator("tbody tr").all();
+  expect(rows.length).toBe(1);
   const row = rows[0];
   if (row === undefined) {
-    throw new Error("Missing row with created upgraded.")
+    throw new Error("Missing row with created upgraded.");
   }
-  await row.getByText("TC202").isVisible()
-  await row.getByText("ACTIVE").isVisible()
-  await row.getByText("View").isVisible()
-})
+  await row.getByText("TC202").isVisible();
+  await row.getByText("ACTIVE").isVisible();
+  await row.getByText("View").isVisible();
+});
 
-test("TC203: Try to create emergency upgrade with invalid calldata -> Cannot be done. Shows error message.", async ({ page }) => {
-  await goToCreateEmergencyProposal(page)
+test("TC203: Try to create emergency upgrade with invalid calldata -> Cannot be done. Shows error message.", async ({
+  page,
+}) => {
+  await goToCreateEmergencyProposal(page);
 
   await page.getByLabel("Title").fill("TC203");
   await page.getByRole("button", { name: "Next" }).click();
@@ -140,54 +145,69 @@ test("TC203: Try to create emergency upgrade with invalid calldata -> Cannot be 
   await page.getByRole("button", { name: "Next" }).click();
   await page.getByText("Error validating transactions").isVisible();
   await page.getByRole("button", { name: "Submit" }).isDisabled();
-})
+});
 
-test("TC204: Security council member approves emergency proposal -> Council count +1, approve button disabled", async ({ page, switcher, wallet }) => {
+test("TC204: Security council member approves emergency proposal -> Council count +1, approve button disabled", async ({
+  page,
+  switcher,
+  wallet,
+}) => {
   await switcher.council(1, page);
   await createEmergencyProposal(page);
   await goToActiveEmergencyProposal(page);
 
   await withVoteIncrease(page, "security-signatures", async () => {
     await approveEmergencyProposal(page, wallet);
-  })
+  });
 
   await expect(approveButton(page)).toBeDisabled();
 
   expect(await extractApprovalCount(page, "guardian-signatures")).toEqual("0/5");
   expect(await extractApprovalCount(page, "zkfoundation-signatures")).toEqual("0/1");
-})
+});
 
-test("TC205: Guardian member approves emergency proposal -> guardian +1, approve button disabled", async ({ page, switcher, wallet }) => {
+test("TC205: Guardian member approves emergency proposal -> guardian +1, approve button disabled", async ({
+  page,
+  switcher,
+  wallet,
+}) => {
   await switcher.guardian(1, page);
   await createEmergencyProposal(page);
   await goToActiveEmergencyProposal(page);
 
   await withVoteIncrease(page, "guardian-signatures", async () => {
     await approveEmergencyProposal(page, wallet);
-  })
+  });
 
   await expect(approveButton(page)).toBeDisabled();
 
   expect(await extractApprovalCount(page, "security-signatures")).toEqual("0/9");
   expect(await extractApprovalCount(page, "zkfoundation-signatures")).toEqual("0/1");
-})
+});
 
-test("TC206: ZkFoundation approves emergency proposal -> zkfoundation +1, approve button disabled", async ({ page, switcher, wallet }) => {
+test("TC206: ZkFoundation approves emergency proposal -> zkfoundation +1, approve button disabled", async ({
+  page,
+  switcher,
+  wallet,
+}) => {
   await switcher.zkFoundation(page);
   await createEmergencyProposal(page);
   await goToActiveEmergencyProposal(page);
 
   await withVoteIncrease(page, "zkfoundation-signatures", async () => {
     await approveEmergencyProposal(page, wallet);
-  })
+  });
 
   await expect(approveButton(page)).toBeDisabled();
 
   expect(await extractApprovalCount(page, "security-signatures")).toEqual("0/9");
   expect(await extractApprovalCount(page, "guardian-signatures")).toEqual("0/5");
-})
+});
 
-test("TC214: Visitor goes to detail page -> Approve button is not shown", async ({ page, switcher, wallet }) => {
+test("TC214: Visitor goes to detail page -> Approve button is not shown", async ({
+  page,
+  switcher,
+}) => {
   await switcher.visitor(page);
   await createEmergencyProposal(page);
   await goToActiveEmergencyProposal(page);
@@ -195,7 +215,7 @@ test("TC214: Visitor goes to detail page -> Approve button is not shown", async 
   await expect(page.getByText("No signing actions")).toBeVisible();
   await expect(page.getByText("Approve emergency upgrade")).not.toBeVisible();
   await expect(approveButton(page)).not.toBeAttached();
-})
+});
 
 async function fillEmergencyUpgrade(page: Page, wallet: Dappwright, switcher: RoleSwitcher) {
   await switcher.council(1);
@@ -205,21 +225,25 @@ async function fillEmergencyUpgrade(page: Page, wallet: Dappwright, switcher: Ro
   await repeatFor(councilRange(1, 9), async (n) => {
     await switcher.council(n, page);
     await approveEmergencyProposal(page, wallet);
-  })
+  });
 
   await switcher.guardian(1, page);
   await goToActiveEmergencyProposal(page);
   await repeatFor(guardianRange(1, 5), async (n) => {
     await switcher.guardian(n, page);
     await approveEmergencyProposal(page, wallet);
-  })
+  });
 
   await switcher.zkFoundation(page);
   await goToActiveEmergencyProposal(page);
   await approveEmergencyProposal(page, wallet);
 }
 
-test("TC207, TC208, TC209, TC210: Filled emergency proposal -> Every rol can see the broadcast button enabled", async ({ page, wallet, switcher }) => {
+test("TC207, TC208, TC209, TC210: Filled emergency proposal -> Every rol can see the broadcast button enabled", async ({
+  page,
+  wallet,
+  switcher,
+}) => {
   await fillEmergencyUpgrade(page, wallet, switcher);
 
   await switcher.council(1, page);
@@ -237,9 +261,13 @@ test("TC207, TC208, TC209, TC210: Filled emergency proposal -> Every rol can see
   await switcher.visitor(page);
   await goToActiveEmergencyProposal(page);
   await expect(page.getByRole("button", { name: "Execute upgrade" })).toBeEnabled();
-})
+});
 
-test("TC211, TC212, TC213: Broadcast emergency proposal -> Shows right txid -> Is shown as inactive -> No more actions available", async ({ page, wallet, switcher }) => {
+test("TC211, TC212, TC213: Broadcast emergency proposal -> Shows right txid -> Is shown as inactive -> No more actions available", async ({
+  page,
+  wallet,
+  switcher,
+}) => {
   await fillEmergencyUpgrade(page, wallet, switcher);
 
   await switcher.council(1, page);
@@ -251,11 +279,11 @@ test("TC211, TC212, TC213: Broadcast emergency proposal -> Shows right txid -> I
   await goToEmergencyIndex(page);
 
   const inactiveCard = page.getByTestId("inactive-proposals-card");
-  await inactiveCard.getByText("Emergency test!!").isVisible()
-  await inactiveCard.getByText("BROADCAST").isVisible()
-  await inactiveCard.getByText("View").click()
+  await inactiveCard.getByText("Emergency test!!").isVisible();
+  await inactiveCard.getByText("BROADCAST").isVisible();
+  await inactiveCard.getByText("View").click();
 
-  await page.getByText("Proposal Details").isVisible()
-  await page.getByRole("button", { name: "Approve emergency upgrade" }).isDisabled()
-  await page.getByRole("button", { name: "Execute upgrade" }).isDisabled()
-})
+  await page.getByText("Proposal Details").isVisible();
+  await page.getByRole("button", { name: "Approve emergency upgrade" }).isDisabled();
+  await page.getByRole("button", { name: "Execute upgrade" }).isDisabled();
+});
