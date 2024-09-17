d5eaab294b7431965e3d46ae828806d58d3d2f26
hojarasca <miguel@moonsonglabs.com>
2024-09-17 09:41:05 -0300

    make TX105 bulletproof.

--- packages/e2e/suites/full/landing.spec.ts
+++ packages/e2e/suites/full/landing.spec.ts
@@ -24,20 +24,27 @@ test("TC103 - Login as zk foundation", async ({ switcher, page }) => {
   await expect(page.getByTestId("user-role")).toHaveText("ZkSync Foundation");
 });
 
-test("TC104 - View all buttons in private app", async ({ page, testApp }) => {
+test("TC104 - View all buttons in private app", async ({ page }) => {
   await expect(page.getByText("Standard Upgrades")).toBeVisible();
   await expect(page.getByText("Emergency Upgrades")).toBeVisible();
   await expect(page.getByText("Freeze Requests")).toBeVisible();
   await expect(page.getByText("L2 Proposals Veto")).toBeVisible();
 });
 
-test("TC105 - View only standard upgrades in private app", async ({ page, testApp }) => {
-  await testApp.resetApp({ env: { ALLOW_PRIVATE_ACTIONS: "false" } });
-
-  await expect(page.getByText("Standard Upgrades")).toBeVisible();
-  await expect(page.getByText("Emergency Upgrades")).not.toBeVisible();
-  await expect(page.getByText("Freeze Requests")).not.toBeVisible();
-  await expect(page.getByText("L2 Proposals Veto")).not.toBeVisible();
-
-  await testApp.resetApp({ env: { ALLOW_PRIVATE_ACTIONS: "true" } });
+test("TC105 - View only standard upgrades in private app", async ({ testApp, context }) => {
+  try {
+    await testApp.resetApp({ env: { ALLOW_PRIVATE_ACTIONS: "false" } });
+    const page = await context.newPage();
+    await page.goto("/");
+    try {
+      await expect(page.getByText("Standard Upgrades")).toBeVisible();
+      await expect(page.getByText("Emergency Upgrades")).not.toBeVisible();
+      await expect(page.getByText("Freeze Requests")).not.toBeVisible();
+      await expect(page.getByText("L2 Proposals Veto")).not.toBeVisible();
+    } finally {
+      await page.close();
+    }
+  } finally {
+    await testApp.resetApp({ env: { ALLOW_PRIVATE_ACTIONS: "true" } });
+  }
 });
