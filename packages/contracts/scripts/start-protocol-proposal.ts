b0caf514969702a94b9b61a1ad61973c6e85d0ea
hojarasca <miguel@moonsonglabs.com>
2024-10-22 17:23:40 -0300

    First scripts working

---
+++ packages/contracts/scripts/start-protocol-proposal.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import { keccak256, numberToHex } from "viem";
+import { EXAMPLE_PROTOCOL_UPGRADE } from "./util/constants.js";
+import { withProtocolGovernor } from "./util/with-protocol-governor.js";
+
+
+async function main() {
+  await withProtocolGovernor(async (contract) => {
+    const response = await contract
+      .getFunction("propose")
+      .send(
+        EXAMPLE_PROTOCOL_UPGRADE.addresses,
+        EXAMPLE_PROTOCOL_UPGRADE.values,
+        EXAMPLE_PROTOCOL_UPGRADE.callDatas,
+        EXAMPLE_PROTOCOL_UPGRADE.description
+      );
+    await response.wait()
+
+    const proposalId = await contract
+      .getFunction("hashProposal")
+      .staticCall(
+        EXAMPLE_PROTOCOL_UPGRADE.addresses,
+        EXAMPLE_PROTOCOL_UPGRADE.values,
+        EXAMPLE_PROTOCOL_UPGRADE.callDatas,
+        keccak256(Buffer.from(EXAMPLE_PROTOCOL_UPGRADE.description))
+      );
+
+    console.log(`Proposed`);
+    console.log(`Id=${numberToHex(proposalId)}`)
+    console.log(`Addresses=${EXAMPLE_PROTOCOL_UPGRADE.addresses}`)
+    console.log(`Values=${EXAMPLE_PROTOCOL_UPGRADE.values}`)
+    console.log(`CallDatas=${EXAMPLE_PROTOCOL_UPGRADE.callDatas}`)
+    console.log(`Description=${EXAMPLE_PROTOCOL_UPGRADE.description}`)
+  })
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+    console.log("❌ failed");
+  });
