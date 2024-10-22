e03414f7dc6c87020c59cb460b2b46a8cc067671
hojarasca <miguel@moonsonglabs.com>
2024-10-22 17:34:18 -0300

    Avoid repeated code

--- packages/contracts/scripts/execute-protocol-proposal.ts
+++ packages/contracts/scripts/execute-protocol-proposal.ts
@@ -1,74 +1,21 @@
-import hre from "hardhat";
-import { Contract, Provider, Wallet } from "zksync-ethers";
-import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import "dotenv/config"
-import { bytesToHex, encodeFunctionData, keccak256, numberToHex } from "viem";
-import { randomBytes } from "node:crypto";
+import { keccak256 } from "viem";
 
-// Workaround for the workaround proposed here: https://docs.zksync.io/build/tooling/hardhat/plugins/hardhat-zksync-node#running-hardhats-test-task-with-hardhat-zksync-node.
-// The import to get the type it's not exposed. This is the part needed to make
-// the typing work.
-declare module "hardhat/types/config" {
-  interface HardhatNetworkConfig {
-    zksync: boolean;
-    url: string;
-  }
-}
-
-const PROTOCOL_GOVERNOR_ADDRESS = "0x08c18e8359C3c6aA600A3726BA6dCC100e222021";
+import { withProtocolGovernor } from "./util/with-protocol-governor.js";
+import { EXAMPLE_PROTOCOL_UPGRADE } from "./util/constants";
 
 async function main() {
-  const privateKey = process.env.PRIV_KEY
-  const proposalId = process.env.PROPOSAL_ID
-
-  if (!privateKey || !proposalId) {
-    throw new Error("Please provide a private key via PRIV_KEY env var.")
-  }
-
-  const provider = new Provider(hre.network.config.url, undefined, { cacheTimeout: -1 });
-  const zkWallet = new Wallet(privateKey, provider)
-  const deployer = new Deployer(hre, zkWallet);
-  const artifact = await deployer.loadArtifact("ZkProtocolGovernor")
-
-  const contract = new Contract(PROTOCOL_GOVERNOR_ADDRESS, artifact.abi, zkWallet)
-
-  const calldata = encodeFunctionData({
-    abi: [
-      {
-        inputs: [
-          {
-            internalType: "bytes",
-            name: "_message",
-            type: "bytes",
-          },
-        ],
-        name: "sendToL1",
-        outputs: [
-          {
-            internalType: "bytes32",
-            name: "hash",
-            type: "bytes32",
-          },
-        ],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ],
-    functionName: "sendToL1",
-    args: [
-      "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000200000000000000000000000000358baca94dcd7931b7ba7aaf8a5ac6090e143a500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000004499a88ec400000000000000000000000035a54c8c757806eb6820629bc82d90e056394c92000000000000000000000000cb4c8d1ecdca0e256a7341c4487938cfaaed432200000000000000000000000000000000000000000000000000000000",
-    ],
-  });
-
-  const propAddresses = ["0x0000000000000000000000000000000000008008"];
-  const propValues = [0n];
-  const propCallDatas = [calldata];
-  const description = `Test protocol proposal 01`;
-
-  const executeTx = await contract
-    .getFunction("execute")
-    .send(propAddresses, propValues, propCallDatas, keccak256(Buffer.from(description)));
-  await executeTx.wait();
+  await withProtocolGovernor(async (contract) => {
+    const executeTx = await contract
+        .getFunction("execute")
+        .send(
+            EXAMPLE_PROTOCOL_UPGRADE.addresses,
+            EXAMPLE_PROTOCOL_UPGRADE.values,
+            EXAMPLE_PROTOCOL_UPGRADE.callDatas,
+            keccak256(Buffer.from(EXAMPLE_PROTOCOL_UPGRADE.description))
+        );
+    await executeTx.wait();
+  })
 
   console.log("Executed OK");
 }
--- packages/contracts/scripts/vote-protocol-proposal.ts
+++ packages/contracts/scripts/vote-protocol-proposal.ts
@@ -1,22 +1,5 @@
-import hre from "hardhat";
-import { Contract, Provider, Wallet } from "zksync-ethers";
-import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import "dotenv/config"
-import { bytesToHex, encodeFunctionData, keccak256, numberToHex } from "viem";
-import { randomBytes } from "node:crypto";
 import { withProtocolGovernor } from "./util/with-protocol-governor";
-
-// Workaround for the workaround proposed here: https://docs.zksync.io/build/tooling/hardhat/plugins/hardhat-zksync-node#running-hardhats-test-task-with-hardhat-zksync-node.
-// The import to get the type it's not exposed. This is the part needed to make
-// the typing work.
-declare module "hardhat/types/config" {
-  interface HardhatNetworkConfig {
-    zksync: boolean;
-    url: string;
-  }
-}
-
-const PROTOCOL_GOVERNOR_ADDRESS = "0x08c18e8359C3c6aA600A3726BA6dCC100e222021";
 
 async function main() {
   const proposalId = process.env.PROPOSAL_ID
